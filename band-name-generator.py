5a24fdc4c2d3f34227fc389ca8097de2ffba6162
leticiachagasg <97345497+leticiachagasg@users.noreply.github.com>
2022-05-25 21:19:46 -0300

    Update band-name-generator.py

--- band-name-generator.py
+++ band-name-generator.py
@@ -17,6 +17,4 @@ city = input("What is the name of the city you grew up in?\n")
 
 pet = input("What is the name of your pet?\n")
 
-band_name = city + ' ' + pet
-
-print(f"Your band name is {band_name}")
+print(f"Your band name is {city} {pet}")
