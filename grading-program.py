7399b154064af5b22d21dc9ede7e6c3731ca0a30
leticiachagasg <97345497+leticiachagasg@users.noreply.github.com>
2022-06-01 09:56:24 -0300

    Update grading-program.py

--- grading-program.py
+++ grading-program.py
@@ -12,11 +12,11 @@ student_grades = {}
 
 #TODO-2: Write your code below to add the grades to student_grades.👇
 for key in student_scores:
-    if student_scores[key] > 90 and student_scores[key] < 101:
+    if student_scores[key] > 90:
         student_grades[key] = "Outstanding"
-    elif student_scores[key] > 80 and student_scores[key] < 91:
+    elif student_scores[key] > 80:
         student_grades[key] = "Exceeds Expectations"
-    elif student_scores[key] > 70 and student_scores[key] < 81:
+    elif student_scores[key] > 70:
         student_grades[key] = "Acceptable"
     elif student_scores[key] <= 70:
         student_grades[key] = "Fail"
